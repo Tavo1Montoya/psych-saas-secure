2662177d9547ed0e322535f63cef784be2fb857a
Tavo1Montoya <saved30@hotmail.com>
2026-02-26 18:47:51 -0600

    Fix typo in router initialization

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-router = APIrouter()
+router = APIRouter()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
